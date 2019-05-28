bd26133f335bdb8957de277f0b3821317fefa4b4
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-05-27 22:10:12 -0700

    both databases complete, tests needed

--- src/middleware/db-model-finder.js
+++ src/middleware/db-model-finder.js
@@ -2,7 +2,5 @@
 
 module.exports = (req, res, next) => {
   req.db = require(`../models/${process.env.DB}-model.js`);
-  // console.log('db-model-finder.js');
-  // console.log(req.db.get());
   next();
 };
